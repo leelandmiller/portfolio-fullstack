be93dfbfdc6c72f543db9ec1546f7bc851a64691
Leeland <leelandmiller@gmail.com>
2017-10-11 21:23:59 -0700

    Production Build

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ router.use('/api', apiRoutes);
 router.use('/auth', authRoutes);
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client','build', 'index.html'));
 });
 
 module.exports = router;
